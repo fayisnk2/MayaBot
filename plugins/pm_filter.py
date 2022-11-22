54b798a6534359629f05fe44b4528c9abd7c566f
Mujahidul •• R7Video <99132878+Mujahidul09@users.noreply.github.com>
2022-11-22 23:33:52 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -407,7 +407,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
             InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('Owner ❤️', url='https://t.me/R7Video')
+            InlineKeyboardButton('Owner ❤️', url='https://t.me/F9Videos')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
