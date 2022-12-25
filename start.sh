d4cf330d03dd0538426b460a663d6d02e61d1ead
Mujahidul •• R7Video <99132878+Mujahidul09@users.noreply.github.com>
2022-12-25 16:17:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Mujahidul09/MayaBot.git /MayaBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /MayaBot
 fi
-cd /EvaMaria
+cd /MayaBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
